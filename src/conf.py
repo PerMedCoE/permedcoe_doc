99c4838a964668e93239182d0b23d1bfb7e93999
fconejer <francisco.conejero@bsc.es>
2021-05-07 10:13:48 +0200

    Disabled edit-on-github link.

--- src/conf.py
+++ src/conf.py
@@ -67,6 +67,11 @@ html_theme = "sphinx_rtd_theme"  # 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['../_static']
 
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    '_templates/custom.css',
+]
 
 html_theme_options = {
     'logo_only': True,
